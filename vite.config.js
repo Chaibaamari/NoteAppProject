5b6a3cd159abe85e657bfa30fdfe75a529ce358a
Chaibaamari <chaibaamari2@gmail.com>
2024-07-09 15:22:58 +0100

    Uploud NoteApp

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // This should be the default, but ensure it's set correctly
-  },
-});
+})
